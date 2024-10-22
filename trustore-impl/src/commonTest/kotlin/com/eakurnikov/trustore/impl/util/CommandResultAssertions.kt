3491513c97f33cf828f42d06d2165acdfffdacb5
eakurnikov <eakurnikov@gmail.com>
2024-10-22 19:04:06 +0400

    Add trustore unit test

---
+++ trustore-impl/src/commonTest/kotlin/com/eakurnikov/trustore/impl/util/CommandResultAssertions.kt
@@ -0,0 +1,16 @@
+package com.eakurnikov.trustore.impl.util
+
+import com.eakurnikov.trustore.api.CommandResult
+import kotlin.test.assertEquals
+
+fun assertSuccess(result: CommandResult<*>) {
+    assertEquals(CommandResult.Status.SUCCESS, result.status)
+}
+
+fun assertFailure(result: CommandResult<*>) {
+    assertEquals(CommandResult.Status.FAILURE, result.status)
+}
+
+fun assertError(result: CommandResult<*>) {
+    assertEquals(CommandResult.Status.ERROR, result.status)
+}
---
+++ trustore-impl/src/commonTest/kotlin/com/eakurnikov/trustore/impl/util/SpyCommands.kt
@@ -0,0 +1,46 @@
+package com.eakurnikov.trustore.impl.util
+
+import com.eakurnikov.trustore.api.CommandResult
+import com.eakurnikov.trustore.api.ControlCommand
+import com.eakurnikov.trustore.api.ReadCommand
+import com.eakurnikov.trustore.api.Store
+import com.eakurnikov.trustore.api.Transactions
+import com.eakurnikov.trustore.api.WriteCommand
+import kotlinx.coroutines.delay
+
+object SpyCommands {
+
+    class Read(
+        private val delayMs: Long,
+        private val onExecute: (() -> Unit)? = null
+    ) : ReadCommand {
+
+        override suspend fun execute(store: Store.Read): CommandResult<String?> {
+            delay(delayMs)
+            onExecute?.invoke()
+            return success("")
+        }
+    }
+
+    class Write(
+        private val delayMs: Long,
+        private val onExecute: (() -> Unit)? = null
+    ) : WriteCommand {
+
+        override suspend fun execute(store: Store.Write): CommandResult<Unit> {
+            delay(delayMs)
+            onExecute?.invoke()
+            return success()
+        }
+    }
+
+    class Control(
+        private val delayMs: Long
+    ) : ControlCommand {
+
+        override suspend fun execute(transactions: Transactions): CommandResult<Unit> {
+            delay(delayMs)
+            return success()
+        }
+    }
+}
